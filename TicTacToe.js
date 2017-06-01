487def78b7c5680b1c98f4f479e90cdaf857006f
Zack <zack.furman32@gmail.com>
2017-05-31 22:14:34 -0400

    Adding css and js

---
+++ TicTacToe.js
@@ -0,0 +1,12 @@
+// TODO:
+	// function clickBox: Selects box as "X" or "O" pending whose turn
+	// function stopClickingBox = Alert user box is already selected
+	// function winner = Stops game. Displays winning player.
+	// function draw = Stops game when all boxes are filled & no winner
+	// function reset = Clears all boxes in table
+	// function newGame = Add score to user
+	// function playerBox = Change color & currentPlayer
+
+$(function() {
+
+})
